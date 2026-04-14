05a461148a400f799f101c470f32e084f6e6c290
lmbattaglia <lmbattag@syr.edu>
2026-04-14 09:33:42 -0400

    Altered permit collection script

--- collect_permits.py
+++ collect_permits.py
@@ -19,11 +19,9 @@ import time
 
 # Identify desired variables
 
-variables = "id,permit_,permit_type,street_number,street_name,work_type,work_description,subtotal_waived,total_fee,census_tract,ward,xcoordinate,ycoordinate"
+variables = "id,permit_,permit_type,application_start_date,issue_date,street_number,street_name,work_type,work_description,subtotal_waived,total_fee,census_tract,ward,xcoordinate,ycoordinate"
 
-# Unauthenticated client only works with public data sets. Note 'None'
-# in place of application token, and no username or password:
-#client = Socrata("data.cityofchicago.org", None)
+# Read app token (from City of Chicago website) with username and password into file
 
     
 with open('apptoken.txt') as fh:
@@ -35,19 +33,25 @@ with open('username.txt') as fh:
 with open('password.txt') as fh:
     password = fh.readline().strip()
 
-# Example authenticated client (needed for non-public datasets):
+# Use city resource example authenticated client (needed for non-public datasets) to pull this dataset
+
 client = Socrata("data.cityofchicago.org",
                   apptoken,
                   username=username,
                   password=password,
                   timeout=120)
 
+# Specify batch size of 50,000 and use offset to pull multiple batches
 
 batch_size = 50000
 all_records = []
 offset = 0
 
+# Indicate that download is beginning
+
 print("Starting download...")
+
+# Define data collection function with correct dataset endpoint
 
 while True:
     batch = client.get(
@@ -62,13 +66,17 @@ while True:
     all_records.extend(batch)
     print(f"  Fetched {len(all_records):,} records so far (batch: {batch_size})")
 
+# Specify endpoint in data collection when batch size = 0
+
     if batch_size == 0:
         break  
+
+# Give time for the API to rest between pulls 
 
     offset += batch_size
     time.sleep(0.5)
 
-# Save as df 
+# Save as dataframe 
 
 df = pd.DataFrame.from_records(all_records)
 df.to_csv("chicago_building_permits.csv", index=False)

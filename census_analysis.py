b22e69048586246b35f0b0c5bfdb2a1c25db949c
lmbattaglia <lmbattag@syr.edu>
2026-04-20 20:50:18 -0400

    Began analysis of census data

--- census_analysis.py
+++ census_analysis.py
@@ -8,3 +8,44 @@ Created on Mon Apr 20 20:18:57 2026
 
 # Visualize census demographic data
 
+import pandas as pd
+import geopandas as gpd
+
+# Read shapefile into variable, filter to NY counties
+
+geodata = gpd.read_file("cb_2024_17_tract_500k.zip")
+geodata = geodata.query('COUNTYFP == "031"')
+
+# Read census data into file
+
+demos = pd.read_csv("chicago_census_data.csv", dtype={"GEOID": str})
+
+# Merge geodata on census data
+
+geodata = geodata.merge(demos, on="GEOID", how="left", indicator=True)
+
+# Print value counts and drop column
+
+print( geodata['_merge'].value_counts() )
+geodata.drop(columns='_merge',inplace=True)
+
+# Write geodata to geopackage file
+
+geodata.to_file("tracts.gpkg",layer="")
+
+# Read chicago city boundary into shapefile
+
+boundary = gpd.read_file("chicago_city_boundary.zip")
+
+# Set up projection number
+
+utm18n = 26918
+
+# Reproject shapefiles to utm 18n
+
+boundary = boundary.to_crs(epsg=utm18n)
+geodata = geodata.to_crs(epsg=utm18n)
+
+# Clip census data to City of Chicago
+
+geo_clip = geodata.clip(boundary,keep_geom_type=True)
--- census_api.py
+++ census_api.py
@@ -18,16 +18,32 @@ import requests
 
 variables = {
     'B19013_001E': 'median_household_income',
-    'B25077_001E': 'median_property_value',
-    'B17001_002E': 'population_below_poverty',
-    'B01003_001E': 'total_population',
-    'B17001_001E': 'poverty_universe',
     'B19301_001E': 'per_capita_income',
+    'B17001_002E': 'population_below_poverty',
+    'B17001_001E': 'poverty_universe',
+    'B25077_001E': 'median_property_value',
     'B25064_001E': 'median_gross_rent',
     'B23025_005E': 'unemployed',
     'B23025_002E': 'labor_force',
-    'B15003_022E': 'bachelors_degree',
-    'B15003_001E': 'education_universe',
+    'B01003_001E': 'total_population',
+    'B02001_002E': 'pop_white_alone',
+    'B02001_003E': 'pop_black_alone',
+    'B02001_004E': 'pop_aian_alone',         
+    'B02001_005E': 'pop_asian_alone',
+    'B02001_006E': 'pop_nhpi_alone',       
+    'B02001_007E': 'pop_other_alone',
+    'B02001_008E': 'pop_two_or_more_races',
+    'B03003_003E': 'pop_hispanic_latino',    
+    'B15003_001E': 'education_universe',    
+    'B15003_017E': 'edu_high_school_diploma',
+    'B15003_018E': 'edu_ged',
+    'B15003_019E': 'edu_some_college_lt1yr',
+    'B15003_020E': 'edu_some_college_gt1yr',
+    'B15003_021E': 'edu_associates',
+    'B15003_022E': 'edu_bachelors',
+    'B15003_023E': 'edu_masters',
+    'B15003_024E': 'edu_professional',
+    'B15003_025E': 'edu_doctorate',
 }
 
 var_string = 'NAME,' + ','.join(variables.keys())
@@ -68,22 +84,49 @@ colnames = row_list[0]
 datarows = row_list[1:]
 cook_demo = pd.DataFrame(columns=colnames,data=datarows)
 
-# Rename variables to nicer names
-
-cook_demo = cook_demo.rename(columns=variables)
-
 # Build GEOID for joining to other datasets
 
 cook_demo["GEOID"] = cook_demo["state"] + cook_demo["county"] + cook_demo["tract"]
+
+# Rename variables to nicer names
+
+cook_demo = cook_demo.rename(columns=variables)
 
 # Convert numeric columns from strings to numbers 
 numeric_cols = list(variables.values())
 cook_demo[numeric_cols] = cook_demo[numeric_cols].apply(pd.to_numeric, errors='coerce')
 
-# ── Derive useful rate columns ────────────────────────────────────────────────
+# Derive useful rate columns
+
+# Poverty and unemployment rates 
+
 cook_demo["poverty_rate"] = cook_demo["population_below_poverty"] / cook_demo["poverty_universe"]
 cook_demo["unemployment_rate"] = cook_demo["unemployed"] / cook_demo["labor_force"]
-cook_demo["pct_bachelors"] = cook_demo["bachelors_degree"] / cook_demo["education_universe"]
+
+# Race shares
+
+cook_demo['pct_white']    = cook_demo['pop_white_alone']    / cook_demo['total_population']
+cook_demo['pct_black']    = cook_demo['pop_black_alone']    / cook_demo['total_population']
+cook_demo['pct_asian']    = cook_demo['pop_asian_alone']    / cook_demo['total_population']
+cook_demo['pct_hispanic'] = cook_demo['pop_hispanic_latino']/ cook_demo['total_population']
+
+# Educational attainment (Percent of pop 25+ with a bachelor's or higher)
+
+cook_demo['pct_bachelors_plus'] = (
+    cook_demo['edu_bachelors'] +
+    cook_demo['edu_masters']   +
+    cook_demo['edu_professional'] +
+    cook_demo['edu_doctorate']
+) / cook_demo['education_universe']
+
+# Percent with less than high school
+
+cook_demo['pct_no_hs'] = 1 - (
+    cook_demo[['edu_high_school_diploma','edu_ged','edu_some_college_lt1yr',
+               'edu_some_college_gt1yr','edu_associates','edu_bachelors',
+               'edu_masters','edu_professional','edu_doctorate']].sum(axis=1)
+    / cook_demo['education_universe']
+)
 
 # Set index of df to be names, write out to CSV file
 
